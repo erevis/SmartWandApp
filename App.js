202f2461a0e6a0cc8fe3670191efea53278a05f8
unknown <nwilen@live.unc.edu>
2021-10-13 16:55:38 -0700

    added bottom taskbar for frontend

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import {StyleSheet, View, Text} from 'react-native'
+import { Entypo , AntDesign  } from '@expo/vector-icons';
+import {Therapy} from './Therapy.js'
+import {Education} from './Education.js'
+import {Progress} from './Progress.js'
+
+
+const Tab = createMaterialBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Therapy"
+      activeColor="#e9cb1e"
+      labelStyle={{ fontSize: 12 }}
+      barStyle={{ backgroundColor: '#202c40' , paddingBottom: 0}}
+    >
+      <Tab.Screen
+        name="Therapy"
+        component={Therapy}
+        options={{
+          tabBarLabel: 'Therapy',
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="medicinebox" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Education"
+        component={Education}
+        options={{
+          tabBarLabel: 'Education',
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="book" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Progress"
+        component={Progress}
+        options={{
+          tabBarLabel: 'Progress',
+          tabBarIcon: ({ color }) => (
+            <Entypo name="progress-two" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Tabs />
+      </NavigationContainer>
   );
 }
 
