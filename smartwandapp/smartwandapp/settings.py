c01ba64e1a688c81cf67dd285baded3f5159f9e9
unknown <nwillen@live.unc.edu>
2021-12-03 21:19:45 -0800

    sqlite test

--- smartwandapp/smartwandapp/settings.py
+++ smartwandapp/smartwandapp/settings.py
@@ -86,15 +86,15 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'sqlite',
         'NAME': 'smartwanddb',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
     }
 }
 
+        # 'USER': 'root',
+        # 'PASSWORD': 'password',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
