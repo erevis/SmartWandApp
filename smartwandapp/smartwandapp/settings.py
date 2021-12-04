68425228b13d8fca9cb76351796c9a9be46357a7
unknown <nwillen@live.unc.edu>
2021-12-03 22:55:09 -0800

    added db creds

--- smartwandapp/smartwandapp/settings.py
+++ smartwandapp/smartwandapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-38$opyc2*^t10^f#=qn3#i8vwbi#(_0ps%8hv$25!h7hxv+#=w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['smart-wand-db.herokuapp.com', '127.0.0.1']
 
