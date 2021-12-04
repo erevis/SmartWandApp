77bba5e977c6bf2c197348dc86c72ff33e57250b
unknown <nwillen@live.unc.edu>
2021-12-04 00:46:18 -0800

    revert backenf

--- smartwandapp/smartwandapp/urls.py
+++ smartwandapp/smartwandapp/urls.py
@@ -3,9 +3,7 @@ from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('account/', include('account.urls')),
+    path('', include('account.urls')),
     path('', include('forum.urls')),
     path('admin/', admin.site.urls),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
