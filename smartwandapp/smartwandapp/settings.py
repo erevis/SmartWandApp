bc4ed88a8ebd2af498cc1e84525b211687883e11
unknown <nwillen@live.unc.edu>
2021-12-03 20:24:05 -0800

    procfile change

--- smartwandapp/smartwandapp/settings.py
+++ smartwandapp/smartwandapp/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
-    'forum',
+    'smartwandapp.account',
+    'smartwandapp.forum',
     'rest_framework',
 ]
 
